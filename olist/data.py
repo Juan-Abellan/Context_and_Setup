fc9db8378796f769d5b7ee3c6e86ef88867c3c4e
Juan-Abellan <abellangarcia.jf@gmail.com>
2023-11-07 17:56:58 +0100

    last version

--- olist/data.py
+++ olist/data.py
@@ -15,26 +15,23 @@ class Olist:
             # Make extensive use of `breakpoint()` to investigate what `__file__` variable is really
         # Hint 2: Use os.path library to construct path independent of Mac vs. Unix vs. Windows specificities
 
-        csv_path = os.path.dirname(__file__)
-        csv_path = os.path.join(csv_path, '../data/csv')
-        #print(csv_path)
-        #pd.read_csv(os.path.join(csv_path, 'olist_sellers_dataset.csv')).head()
+        csv_path1 = os.path.dirname(__file__)
+        csv_path = os.path.join(csv_path1, '../data/csv')
 
         file_names = []
         for file in os.listdir(csv_path):
             if file.endswith(".csv"):
                 file_names.append(file)
-        file_names
+
 
         key_names = []
         for file in file_names:
             clean_file = file.replace("_dataset.csv", "").replace(".csv", "").replace("olist_", "")
             key_names.append(clean_file)
-        key_names
+
 
         data = {}
         for key, file in zip(key_names, file_names):
-            #print(key, file)
             data[key] = pd.read_csv(os.path.join(csv_path, file))
 
         return data
@@ -44,3 +41,5 @@ class Olist:
         You call ping I print pong.
         """
         print("pong")
+
+# print(Olist().get_data()['sellers'].head())
